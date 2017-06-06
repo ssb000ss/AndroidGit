ab2a4e9bd8fdc4a04697fd3bf981d108a05eb00b
ssb000ss <ssb000ss@gmail.com>
2017-06-06 09:54:37 +0600

    first commit

---
+++ app/src/main/java/com/example/ssb000ss/task_01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ssb000ss.task_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        System.out.println("Privet");
+    }
+}
